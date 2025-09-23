e4e8caca7d08f6c1986f2dc0a6d1508c542c4bc3
ZakariyaArale <arale.zakariya123@gmail.com>
2025-09-22 22:18:15 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Driver {
     public static void main(String [] args) {
         Polynomial p = new Polynomial();
@@ -7,10 +10,30 @@ public class Driver {
         double [] c2 = {0,-2,0,0,-9};
         Polynomial p2 = new Polynomial(c2);
         Polynomial s = p1.add(p2);
+        Polynomial m = p1.multiply(p2);
+        
         System.out.println("s(0.1) = " + s.evaluate(0.1));
+        for(int i = 0; i < m.poly_non_zeros.length; i++)
+        {
+        		System.out.println(m.poly_non_zeros[i]);
+        }
+        System.out.println("m(2) = " + m.evaluate(2));
         if(s.hasRoot(1))
             System.out.println("1 is a root of s");
         else
             System.out.println("1 is not a root of s");
+        try
+        {
+        		Polynomial ms = new Polynomial(new File("sample.txt"));
+        		for(double x:ms.poly_non_zeros) System.out.println(x);
+        		
+        }
+        catch(FileNotFoundException e)
+        {
+        		System.out.println("Can't open file ;(");
+        }
+        
+        
+        
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,64 +1,162 @@
-import java.util.Arrays;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.PrintWriter;
 
 public class Polynomial
 {
-    private double[] poly_val;
-
+    public double[] poly_non_zeros;
+    public int[] poly_exp;
+   
     public Polynomial()
     {
-        double[] zero_poly = {0};
-        poly_val = zero_poly;
+    	poly_non_zeros = new double[] {0};
+        poly_exp = new int[] {0};
     }
-
+    
+    //Helper method
+    private int size_poly(double[] given_poly)
+    {
+		int size = 0;
+		for(int i = 0; i < given_poly.length; i++)
+		{
+			if(given_poly[i] != 0) size++;
+		}
+		return size;
+    }
+    
+    
     public Polynomial(double[] given_poly)
     {
-    	//This is to ensure the array given can't be modified
-        //outside of this class, data encapsulation ;)
-    	//Bit mem inefficient but security is more important (reasonable assumption)
-        poly_val = Arrays.copyOf(given_poly, given_poly.length);
+		int size = size_poly(given_poly);
+		int index = 0;
+		
+		poly_non_zeros = new double[size];
+		poly_exp = new int[size];
+		
+		for(int i = 0; i < given_poly.length; i++)
+		{
+			if(given_poly[i] != 0)
+			{
+				poly_non_zeros[index] = given_poly[i];
+				poly_exp[index] = i;
+				index++;
+			}
+		}
     }
-
+    
+   public Polynomial(File given_file) throws FileNotFoundException 
+   {
+		Scanner sc = new Scanner(given_file);
+		String input = sc.nextLine();
+		sc.close();
+		
+		String[] segments = input.split("(?=[+-])"); 
+		int size_new = Integer.parseInt(segments[segments.length - 1].split("x")[1]) + 1;
+		double[] new_values = new double[size_new];
+		
+		for(int i = 0; i < segments.length; i++)
+		{
+			String[] parts = segments[i].split("x");
+			if(parts.length == 1) new_values[0] += Double.parseDouble(parts[0]);
+			else new_values[Integer.parseInt(parts[1])] += Double.parseDouble(parts[0]);
+		}
+		
+		int size = size_poly(new_values);
+		poly_non_zeros = new double[size];
+		poly_exp = new int[size];
+		int index = 0;
+		for(int i = 0; i < new_values.length; i++) 
+		{
+			if(new_values[i] != 0) 
+			{
+				poly_non_zeros[index] = new_values[i];
+				poly_exp[index] = i;
+				index++;
+			}
+		}
+	} 
+		
+	
+    //Helper method
+    private int index_of_val(int[] given_exp, int val)
+    {
+    		for(int i = 0; i < given_exp.length; i++)
+    		{
+    			if(given_exp[i] == val) return i;
+    		}
+    		return -1;
+    }
+    
     public Polynomial add(Polynomial given_poly)
     {
-        double[] given_array = given_poly.poly_val; //For readability purposes
-        double[] new_values = new double[Math.max(poly_val.length, given_array.length)]; //Take the larger array size
-        int lowest_size = Math.min(poly_val.length, given_array.length);  //This ensures that the "shared" size is within bounds
-        for(int i = 0; i < new_values.length; i++) 
-        {
-            if(i < lowest_size) 
-            {
-                new_values[i] = poly_val[i] + given_array[i];
-            }
-            //Checks if poly_val is the bigger array, if so copy its data
-            else if(poly_val.length == new_values.length)
-            {
-                new_values[i] = poly_val[i];
-            }
-            //Ditto for given_array
-            else
-            {
-                new_values[i] = given_array[i];
-            }
-        }
-        return new Polynomial(new_values); //Return the sum of the polys
+    		
+    	int size = Math.max(poly_exp[poly_exp.length - 1], given_poly.poly_exp[given_poly.poly_exp.length - 1]) + 1;
+     	double[] new_values = new double[size];
+     	for(int i = 0; i < size; i++)
+     	{
+     		int index_i_poly = index_of_val(poly_exp, i);
+     		int index_i_given = index_of_val(given_poly.poly_exp, i);
+     		if(index_i_poly != -1)
+     			new_values[i] += poly_non_zeros[index_i_poly];
+     		else if(index_i_given != -1)
+     			new_values[i] += given_poly.poly_non_zeros[index_i_given];
+     	}
+    		return new Polynomial(new_values);
     }
 
     public double evaluate(double val)
     {
         //Simple loop just add up all the values
         double sum_poly = 0;
-        for(int i = 0; i < poly_val.length; i++)
+        for(int i = 0; i < poly_non_zeros.length; i++)
         {
-            sum_poly += (poly_val[i] * Math.pow(val, i));
+            sum_poly += (poly_non_zeros[i] * Math.pow(val, poly_exp[i]));
         }
         return sum_poly;
     }
 
     public boolean hasRoot(double val)
     {
-        //has root eqv does it evaluate to 0 at val, reasonable assumpution this wont be tested 
-        //with very very small values or that will be mean :_( 
         return evaluate(val) == 0;
     }
+    
+    
+    public Polynomial multiply(Polynomial given_poly)
+    {
+    	int size = poly_exp[poly_exp.length - 1] + given_poly.poly_exp[given_poly.poly_exp.length - 1] + 1;
+    		double[] new_values = new double[size];
+    		for(int i = 0; i < poly_exp.length; i++)
+    		{
+    			for(int j = 0; j < given_poly.poly_exp.length; j++)
+    			{
+    				double val = poly_non_zeros[i] * given_poly.poly_non_zeros[j];
+    				int index = poly_exp[i] + given_poly.poly_exp[j];
+    				new_values[index] += val;
+    			}
+    		}
+    		return new Polynomial(new_values);
+    		
+    }
+    
+    public void saveToFile(String path) throws FileNotFoundException
+    {
+    		PrintWriter dest = new PrintWriter(path);
+    		boolean is_first = true;
+    	for(int i = 0; i < poly_non_zeros.length; i++)
+		{
+			if(poly_exp[i] == 0) dest.print(poly_non_zeros[i]); 
+			else if(poly_non_zeros[i] < 0 || is_first)
+			{
+				dest.print(poly_non_zeros[i] + "x" + poly_exp[i]);
+				is_first = false;
+			}
+			else dest.print("+" + poly_non_zeros[i] + "x" + poly_exp[i]);
+	
+		}
+    		dest.close();
+    }
+    
+    
 
 }
